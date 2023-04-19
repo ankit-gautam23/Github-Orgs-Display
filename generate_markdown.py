071494c6ad4d29c6adf60bd0bd3f1f5b76a1b2fd
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-19 21:44:56 +0530

    Create generate_markdown.py

---
+++ generate_markdown.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+orgs_file = sys.argv[1]
+
+with open(orgs_file, 'r') as f:
+    orgs = json.load(f)
+
+org_links = [f"- [{org['login']}]({org['html_url']})" for org in orgs]
+
+markdown_content = f"""### Contributions to Organizations
+
+{'\n'.join(org_links)}
+"""
+
+print(markdown_content)
